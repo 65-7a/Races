5ac42391b1e671ab3dc484d5cd0eb4358b498920
65_7a <79430821+65-7a@users.noreply.github.com>
2021-10-11 10:21:51 +1100

    Initial commit

---
+++ src/main/java/com/callumwong/classes/Classes.java
@@ -0,0 +1,15 @@
+package com.callumwong.classes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Classes extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
