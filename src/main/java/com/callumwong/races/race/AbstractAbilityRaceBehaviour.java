254d4ff8298e10cf2b135bca23bc0934999c3abe
65_7a <79430821+65-7a@users.noreply.github.com>
2021-10-19 10:40:31 +1100

    Fix inventory check

--- src/main/java/com/callumwong/races/race/AbstractAbilityRaceBehaviour.java
+++ src/main/java/com/callumwong/races/race/AbstractAbilityRaceBehaviour.java
@@ -33,14 +33,14 @@ public abstract class AbstractAbilityRaceBehaviour extends AbstractRaceBehaviour
     }
 
     private void checkInventory(Player player) {
-        abilities().values().forEach(s -> {
-            if (Arrays.stream(Arrays.copyOfRange(player.getInventory().getContents(), 9, 35)).anyMatch(itemStack -> itemStack != null
+        Arrays.stream(Arrays.copyOfRange(player.getInventory().getContents(), 9, 35)).forEach(itemStack -> {
+            if (abilities().values().stream().anyMatch(s -> itemStack != null
                     && itemStack.getItemMeta() != null && itemStack.getItemMeta().getLocalizedName().equals(s))) {
                 ItemStack clone = player.getInventory().getContents()[8].clone();
                 player.getInventory().setItem(8, RacesItems.getEnderianPearl());
                 if (player.getInventory().getContents()[8] != null) {
-                    player.getInventory().addItem(clone).forEach((integer, itemStack) -> {
-                        player.getWorld().dropItem(player.getLocation(), itemStack);
+                    player.getInventory().addItem(clone).forEach((integer, stack) -> {
+                        player.getWorld().dropItem(player.getLocation(), stack);
                     }); // try to add the item to the player's inv, if not drop it to the floor
                 }
             }
